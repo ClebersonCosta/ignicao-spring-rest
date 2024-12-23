fb4b10245fd826bd73379e889eb82e913beb0b6d
ClebersonCosta <cleberson-costa@outlook.com>
2024-12-23 14:35:29 -0300

    feat: implementado consulta dinamica com JPQL

--- src/main/java/com/algaworks/algatransito/api/controller/ProprietarioController.java
+++ src/main/java/com/algaworks/algatransito/api/controller/ProprietarioController.java
@@ -1,29 +1,21 @@
 package com.algaworks.algatransito.api.controller;
 
 import com.algaworks.algatransito.domain.model.Proprietario;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Arrays;
 import java.util.List;
 
 @RestController
 public class ProprietarioController {
 
+    @PersistenceContext
+    private EntityManager manager;
+
     @GetMapping("/proprietarios")
     public List<Proprietario> listar() {
-        var p1 = new Proprietario();
-        p1.setId(1L);
-        p1.setNome("João");
-        p1.setTelefone("11 99999-8888");
-        p1.setEmail("joao@algaworks.com.br");
-
-        var p2 = new Proprietario();
-        p2.setId(2L);
-        p2.setNome("Maria");
-        p2.setTelefone("11 99999-7777");
-        p2.setEmail("maria@algaworks.com.br");
-
-        return Arrays.asList(p1,p2);
+        return manager.createQuery("from Proprietario", Proprietario.class).getResultList();
     }
 }
