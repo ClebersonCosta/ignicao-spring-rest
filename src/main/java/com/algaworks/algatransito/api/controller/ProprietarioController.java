c94220da13ffbd70bc4eb699f43db78874290cbb
ClebersonCosta <cleberson-costa@outlook.com>
2024-12-27 00:24:50 -0300

    feat: implementado buscar por id

--- src/main/java/com/algaworks/algatransito/api/controller/ProprietarioController.java
+++ src/main/java/com/algaworks/algatransito/api/controller/ProprietarioController.java
@@ -3,25 +3,31 @@ package com.algaworks.algatransito.api.controller;
 import com.algaworks.algatransito.domain.model.Proprietario;
 import com.algaworks.algatransito.domain.repository.ProprietarioRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @AllArgsConstructor
 @RestController
+@RequestMapping("/proprietarios")
 public class ProprietarioController {
 
     private final ProprietarioRepository repository;
 
-    @GetMapping("/proprietarios")
+    @GetMapping("/")
     public List<Proprietario> listarTodos() {
         return repository.findAll();
     }
 
-    @GetMapping("/proprietarios/{nome}")
-    public List<Proprietario> listarPorNome(@PathVariable(value = "nome") String nome) {
-        return repository.findByNomeContaining(nome);
+    @GetMapping("/{id}")
+    public ResponseEntity<Proprietario> buscar(@PathVariable Long id) {
+        return repository.findById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
     }
+
 }
