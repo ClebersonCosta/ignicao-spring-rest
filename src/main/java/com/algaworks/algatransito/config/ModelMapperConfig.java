472af15dad8f0bed117f2a08d46d9ceff99f62d0
ClebersonCosta <cleberson-costa@outlook.com>
2025-01-10 01:36:05 -0300

    refactor: adicionado modelmapper para transformar os objetos

---
+++ src/main/java/com/algaworks/algatransito/config/ModelMapperConfig.java
@@ -0,0 +1,23 @@
+package com.algaworks.algatransito.config;
+
+import com.algaworks.algatransito.api.model.VeiculoModel;
+import com.algaworks.algatransito.domain.model.Veiculo;
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ModelMapperConfig {
+
+    @Bean
+    public ModelMapper modelMapper() {
+        var modelMapper = new ModelMapper();
+
+        // mapeamento de propriedades, pois o atributo placa na classe VeiculoModel tem um nome diferente
+        modelMapper.createTypeMap(Veiculo.class, VeiculoModel.class)
+                .addMapping(Veiculo::getPlaca, VeiculoModel::setNumeroPlaca);
+
+        return modelMapper;
+    }
+
+}
